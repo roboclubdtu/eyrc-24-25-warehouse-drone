e63f3b812f71a227e9cfd32eb5cb7ac366806881
Jonathan Mikler <ionymikler@gmail.com>
2024-11-05 09:50:16 +0000

    waypoint service finished

--- swift_pico/src/waypoint_service.py
+++ swift_pico/src/waypoint_service.py
@@ -1,47 +1,60 @@
 #!/usr/bin/env python3
-# WD_4122
-
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseArray
 from waypoint_navigation.srv import GetWaypoints
 
-class WayPoints(Node):
+def coords_list_to_pose(coords):
+    pose_array = PoseArray()
+    for x, y, z in coords:
+        pose = Pose()
+        pose.position.x = x
+        pose.position.y = y
+        pose.position.z = z
+        pose.orientation.w = 1.0  # Default orientation
+        pose_array.poses.append(pose)
+    return pose_array
 
+class WaypointsServer(Node):
     def __init__(self):
-        super().__init__('waypoints_service')
-        self.srv = self.create_service(GetWaypoints, 'waypoints', self.waypoint_callback)
-        self.waypoints = [[2.0, 2.0, 27.0], [2.0, -2.0, 27.0], [-2.0, -2.0, 27.0], [-2.0, 2.0, 27.0], [1.0, 1.0, 27.0]]
+        super().__init__('waypoints_server')
+        self.srv = self.create_service(GetWaypoints, 'GetWaypoints', self.get_waypoints_callback)
+        self.get_logger().info("WaypointsServer node has been started.")
 
-    
-    def waypoint_callback(self, request, response):
-
-        if request.get_waypoints == True :
-            response.waypoints.poses = [Pose() for _ in range(len(self.waypoints))]
-            for i in range(len(self.waypoints)):
-                response.waypoints.poses[i].position.x = self.waypoints[i][0]
-                response.waypoints.poses[i].position.y = self.waypoints[i][1]
-                response.waypoints.poses[i].position.z = self.waypoints[i][2]
-            self.get_logger().info("Incoming request for Waypoints")
-            return response
-
+    def get_waypoints_callback(self, request:GetWaypoints.Request, response:GetWaypoints.Response):
+        if request.get_waypoints:
+            response.waypoints = self.get_waypoints()
+            self.get_logger().info("Waypoints have been successfully sent.")
         else:
-            self.get_logger().info("Request rejected")
+            self.get_logger().info("Request received but 'get_waypoints' flag is False.")
+        return response
 
-def main():
-    rclpy.init()
-    waypoints = WayPoints()
+    def get_waypoints(self):
+        pose_array = PoseArray()
+        # Populate the waypoints
+        coordinates = [
+            [2.0, 2.0, 27.0],
+            [2.0, -2.0, 27.0],
+            [-2.0, -2.0, 27.0],
+            [-2.0, 2.0, 27.0],
+            [1.0, 1.0, 27.0]
+        ]
+
+        pose_array = coords_list_to_pose(coordinates)
+
+        return pose_array
+
+def main(args=None):
+    rclpy.init(args=args)
+    waypoints_server = WaypointsServer()
 
     try:
-        rclpy.spin(waypoints)
+        rclpy.spin(waypoints_server)
     except KeyboardInterrupt:
-        waypoints.get_logger().info('KeyboardInterrupt, shutting down.\n')
+        waypoints_server.get_logger().info("WaypointsServer node has been interrupted.")
     finally:
-        waypoints.destroy_node()
+        waypoints_server.destroy_node()
         rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-        
-
-        
